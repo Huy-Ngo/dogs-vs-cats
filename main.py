170d0ec93395258ee18c7e9547cb6e207c92a4cd
Ngô Ngọc Đức Huy <duchuy29092000@gmail.com>
2021-01-03 22:43:58 +0700

    Try to create the model for the first time (#1)
    * Print summary
    
    * Try to run main
    
    * Rename directory to be consistent
    
    * Attempt at training model
    
    * Add action to save artifact
    
    * Ignore output model
    
    * Add matplotlib to installation
    
    * Save both figures
    
    * Upload both artifacts
    
    * Add ouput folder
    
    * Output to output
    
    * Clear figure after saving

--- main.py
+++ main.py
@@ -1,11 +1,87 @@
-from keras import layers
-from keras import models
+from os.path import dirname, abspath, join
+
+from keras import layers, models, optimizers
+from keras.preprocessing.image import ImageDataGenerator
+
 
 model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+
+# Convolution layers
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
 
+# Flattening and dense layer
+model.add(layers.Flatten())
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
 
+print()
+print(model.summary())
+print(model.compile(loss='binary_crossentropy',
+                    optimizer=optimizers.RMSprop(lr=1e-4),
+                    metrics=['acc']))
+
+current_dir = abspath(dirname(__name__))
+train_dir = join(current_dir, 'train_set')
+test_dir = join(current_dir, 'test_set')
+validation_dir = join(current_dir, 'validation_set')
+cat_train_dir = join(current_dir, 'cats')
+cat_test_dir = join(current_dir, 'cats')
+cat_validation_dir = join(current_dir, 'cats')
+dog_train_dir = join(current_dir, 'dogs')
+dog_test_dir = join(current_dir, 'dogs')
+dog_validation_dir = join(current_dir, 'dogs')
+
+train_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
+
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=100,
+    epochs=30,
+    validation_data=validation_generator,
+    validation_steps=50)
+
+model.save('cats_and_dogs_small_1.h5')
+
+import matplotlib.pyplot as plt
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+
+val_loss = history.history['val_loss']
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.savefig('output/acc_train_validation.png')
+plt.clf()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.savefig('output/loss_train_validation.png')

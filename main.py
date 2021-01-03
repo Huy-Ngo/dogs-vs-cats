d0c566456ef854e57eb70661489bbf4306dd25c4
Huy-Ngo <duchuy29092000@gmail.com>
2021-01-03 17:41:20 +0700

    Some barebone for the script (it didn't do anything)

---
+++ main.py
@@ -0,0 +1,11 @@
+from keras import layers
+from keras import models
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+

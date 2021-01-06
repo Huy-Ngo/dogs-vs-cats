2753348d0473157d8b5a2797d09cbc649cf7da16
nmhoangg2000 <45127849+nmhoangg2000@users.noreply.github.com>
2021-01-06 10:58:52 +0700

    Use data augmentation to improve the data

--- main.py
+++ main.py
@@ -4,6 +4,36 @@ from keras import layers, models, optimizers
 from keras.preprocessing.image import ImageDataGenerator
 
 
+datagen = ImageDataGenerator(
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest')
+
+
+# from keras.preprocessing import image
+
+# fnames = [os.path.join(train_cats_dir, fname) for
+#     fname in os.listdir(train_cats_dir)]
+
+# img_path = fnames[3]
+
+# img = image.load_img(img_path, target_size=(150, 150)
+
+
+# x = image.img_to_array(img)
+# x = x.reshape((1,) + x.shape)i = 0
+# for batch in datagen.flow(x, batch_size=1):
+#     plt.figure(i)
+#     imgplot = plt.imshow(image.array_to_img(batch[0]))
+#     i += 1
+#     if i % 4 == 0:
+#         break
+# plt.show()
+
 model = models.Sequential()
 
 # Convolution layers
@@ -18,6 +48,7 @@ model.add(layers.MaxPooling2D((2, 2)))
 
 # Flattening and dense layer
 model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
 model.add(layers.Dense(512, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
@@ -38,25 +69,25 @@ dog_train_dir = join(current_dir, 'dogs')
 dog_test_dir = join(current_dir, 'dogs')
 dog_validation_dir = join(current_dir, 'dogs')
 
-train_datagen = ImageDataGenerator(rescale=1./255)
+train_datagen = ImageDataGenerator(rescale=1./255,rotation_range=40,width_shift_range=0.2,height_shift_range=0.2,shear_range=0.2,zoom_range=0.2,horizontal_flip=True,)
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(
     train_dir,
     target_size=(150, 150),
-    batch_size=20,
+    batch_size=32,
     class_mode='binary')
 
 validation_generator = test_datagen.flow_from_directory(
     validation_dir,
     target_size=(150, 150),
-    batch_size=20,
+    batch_size=32,
     class_mode='binary')
 
 history = model.fit_generator(
     train_generator,
     steps_per_epoch=100,
-    epochs=30,
+    epochs=100,
     validation_data=validation_generator,
     validation_steps=50)
 
@@ -83,5 +114,8 @@ plt.plot(epochs, loss, 'bo', label='Training loss')
 plt.plot(epochs, val_loss, 'b', label='Validation loss')
 plt.title('Training and validation loss')
 plt.legend()
-
+plt.show()
 plt.savefig('output/loss_train_validation.png')
+
+
+
